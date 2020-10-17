dfac52da5efc6585759745ce7a4f0f541b3e2240
Kleyton72 <jeanc.1582@gmail.com>
2020-10-17 15:41:33 -0400

    Nuevo contexto

--- DAL/Context.cs
+++ DAL/Context.cs
@@ -5,8 +5,9 @@ using RegistroPrestamos.Entities;
 namespace RegistroPrestamos.DAL
 {
     public class Context : DbContext{
-        public DbSet<Persona> Personas { get; set; }
-        public DbSet<Prestamo> Prestamos { get; set; }
+        public DbSet<Personas> Personas { get; set; }
+        public DbSet<Prestamos> Prestamos { get; set; }
+        public DbSet<Moras> Moras { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
